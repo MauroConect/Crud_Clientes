435fdd95657be3d4f34be9bdb925f0381f20d893
unknown <maurot970@gmail.com>
2021-04-24 17:10:23 -0300

    subiendo

--- view/login/login.php
+++ view/login/login.php
@@ -8,15 +8,15 @@
         <title>Login con  PHP - Bootstrap 4</title>
 
         
-        <link rel="stylesheet" href="/plugins/sweetalert2/sweetalert2.min.css">        
-        <link rel="stylesheet" href="/estilos.css">
-        <link rel="stylesheet" type="text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
-        <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../plugins/sweetalert2/sweetalert2.min.css">        
+        <link rel="stylesheet" href="../../estilos.css">
+        <link rel="stylesheet" type="../..//text/css" href="fuentes/iconic/css/material-design-iconic-font.min.css">
+        <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
         
     </head>
     
     <body>
-     
+
       <div class="container-login">
         <div class="wrap-login">
             <form class="login-form validate-form" id="formLogin" action="" method="post">
@@ -43,11 +43,11 @@
     </div>     
         
         
-     <script src="jquery/jquery-3.3.1.min.js"></script>    
-     <script src="bootstrap/js/bootstrap.min.js"></script>    
-     <script src="popper/popper.min.js"></script>    
+     <script src="../../jquery/jquery-3.3.1.min.js"></script>    
+     <script src="../../bootstrap/js/bootstrap.min.js"></script>    
+     <script src="../../popper/popper.min.js"></script>    
         
-     <script src="plugins/sweetalert2/sweetalert2.all.min.js"></script>    
-     <script src="codigo.js"></script>    
+     <script src="../../plugins/sweetalert2/sweetalert2.all.min.js"></script>    
+     <script src="../../codigo.js"></script>    
     </body>
 </html>
